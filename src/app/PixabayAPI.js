e84462bfbdaae26073209cd5c00e71407103d2c3
CherniukAnna <chernukanna@gmail.com>
2023-02-05 14:29:38 +0200

    add gallery

---
+++ src/app/PixabayAPI.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+const API_KEY = '33369262-276cb4d02c9642aa211ce26f1';
+
+export class PixabayAPI {
+    #page = 1;
+    #per_page = 40;
+    #query = '';
+    #totalPages = 0;
+
+    async getPhotos() {
+        const params = {
+            page: this.#page,
+            q: this.#query,
+            per_page: this.#per_page,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: true,
+        }
+
+
+    
+        const urlAXIOS = `?key=${API_KEY}&q=${this.#query}&page=${this.#page}&per_page=${this.#per_page}`;
+
+        const { data } = await axios.get(urlAXIOS, { params, });
+        return data;
+    }
+
+    get query() {
+        this.#query;
+    }
+
+    set query(newQuery) {
+        this.#query = newQuery;
+    }
+
+    incrementPage() {
+        this.#page += 1;
+    }
+
+    resetPage() {
+        this.#page = 1;
+    }
+
+    setTotal(total) {
+        this.#totalPages = total;
+    }
+
+    hasMorePhotos() {
+        return this.#page < Math.ceil(this.#totalPages / this.#per_page);
+    }
+}
